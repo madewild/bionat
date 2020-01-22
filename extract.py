c27251c1f929545a5c192bb75be43c1af72a18b6
madewild <madewild@ulb.ac.be>
2020-01-22 11:17:15 +0100

    add nbn

--- extract.py
+++ extract.py
@@ -1,15 +1,25 @@
 """Extracting PDFs from website"""
 
+import sys
 import requests
 
-for n in range(2037, 2103):
-    url = f"http://www.academieroyale.be/Academie/documents/FichierPDFBiographieNationaleTome{n}.pdf"
+coll = sys.argv[1]
+
+if coll == "bn":
+    nrange = range(2037, 2103)
+    base = "BiographieNationaleTome"
+elif coll == "nbn":
+    nrange = range(2103, 2120)
+    base = "NouvelleBiographieNational"
+else:
+    print("Unknown collection")
+    sys.exit()
+
+for n in nrange:
+    url = f"http://www.academieroyale.be/Academie/documents/FichierPDF{base}{n}.pdf"
     r = requests.get(url)
     if r.status_code == 200:
-        with open(f"data/bn/{n}.pdf", "wb") as f:
+        with open(f"data/{coll}/{n}.pdf", "wb") as f:
             f.write(r.content)
     else:
         print(f"{n}: {r.status_code}")
-
-"""for n in range(2103, )
-    url = f"http://www.academieroyale.be/Academie/documents/FichierPDFNouvelleBiographieNational{n}.pdf"""
