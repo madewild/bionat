97908510e55fc0fe963cce38e7fb0dbabffd7e32
madewild <madewild@ulb.ac.be>
2020-02-08 14:34:54 +0100

    eval ocr

---
+++ ocr.py
@@ -0,0 +1,19 @@
+"""Evaluate OCR quality"""
+
+import os
+import PyPDF2
+from random import seed, randint
+
+seed(42)
+
+root = "data/nbn/"
+for fn in sorted(os.listdir(root)):
+    filename = root+fn
+    pdf_file = open(filename, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+    nb_pages = pdf_reader.numPages
+    n = randint(0, nb_pages-1)
+    page_obj = pdf_reader.getPage(n)
+    text = page_obj.extractText()
+    with open(f"data/ocr/{fn[:-4]}_{n}.txt", "w") as output:
+        output.write(text)
