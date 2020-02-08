f19353a743e8aec76efcac05097671f43184feb9
madewild <madewild@ulb.ac.be>
2020-02-08 15:57:11 +0100

    two roots at once

--- ocr.py
+++ ocr.py
@@ -6,16 +6,15 @@ from random import seed, randint
 
 seed(42)
 
-root = "data/bn/"
-#root = "data/nbn/"
-
-for fn in sorted(os.listdir(root)):
-    filename = root+fn
-    pdf_file = open(filename, 'rb')
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-    nb_pages = pdf_reader.numPages
-    n = randint(10, nb_pages-1)
-    page_obj = pdf_reader.getPage(n)
-    text = page_obj.extractText()
-    with open(f"data/ocr/raw/{fn[:-4]}_{n}.txt", "w") as output:
-        output.write(text)
+roots = ["data/bn/", "data/nbn/"]
+for root in roots:
+    for fn in sorted(os.listdir(root)):
+        filename = root+fn
+        pdf_file = open(filename, 'rb')
+        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        nb_pages = pdf_reader.numPages
+        n = randint(10, nb_pages-1)
+        page_obj = pdf_reader.getPage(n)
+        text = page_obj.extractText()
+        with open(f"data/ocr/new/{fn[:-4]}_{n}.txt", "w") as output:
+            output.write(text)
